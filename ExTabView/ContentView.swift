d57b709b517d75e03f0ebb35023ffb693b01f811
jake <palatable7@naver.com>
2022-08-21 22:21:12 +0900

    Implement

--- ExTabView/ContentView.swift
+++ ExTabView/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+    TabView {
+      ViewA()
+        .badge(2)
+        .tabItem {
+          Label("View1", systemImage: "tray.and.arrow.down.fill")
+        }
+      ViewB()
+        .tabItem {
+          Label("View2", systemImage: "tray.and.arrow.up.fill")
+        }
+      ViewC()
+        .badge("!")
+        .tabItem {
+          Label("View3", systemImage: "person.crop.circle.fill")
+        }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+struct ViewA: View {
+  var body: some View {
+    Text("ViewA")
+  }
+}
+
+struct ViewB: View {
+  var body: some View {
+    Text("ViewB")
+  }
+}
+
+struct ViewC: View {
+  var body: some View {
+    Text("ViewC")
+  }
 }
