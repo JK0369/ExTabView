c5624f55290b7ae40f77cea739eb9a3b1d3da698
jake <palatable7@naver.com>
2022-08-21 22:15:13 +0900

    Initial Commit

---
+++ ExTabView/ExTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExTabViewApp.swift
+//  ExTabView
+//
+//  Created by 김종권 on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ExTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
